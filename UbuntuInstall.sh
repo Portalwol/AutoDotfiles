38c5fe118e7d05056671ba22e08edc5adf1c3536
Wol <88345210+Portalwol@users.noreply.github.com>
2022-01-05 21:04:44 -0800

    Add files via upload

---
+++ UbuntuInstall.sh
@@ -0,0 +1,12 @@
+cr="\033[1;31m"
+cg="\033[1;32m"
+cb="\033[1;34m"
+printf "${cr}[*] Proceeding Will Overwrite Your Current Awesome Config, Make a Backup If your Not Sure About This.\n"
+printf "${cg}[*] Installing dependencies\n"
+sudo apt install awesome rofi feh flameshot mousepad arc-theme kitty lm_sensors
+printf "${cg}[*] Copying Dotfiles\n"
+printf "${cb} [*] Copying Configs\n"
+cp -r config/* ~/.config
+cp -r home/.*rc ~/.
+printf "${cg}[+] Installation Complete Reboot and Change To Awesome In Your Display Manager Enjoy!\n"
+
